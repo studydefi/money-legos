965e01ed74a40874b4892d9d81e3f573b4832a5a
Kendrick Tan <kendricktan0814@gmail.com>
2020-04-14 12:29:27 +1000

    abi, ISoloMargin, addresses

---
+++ src/dydx/contracts/DydxFlashloanBase.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./ISoloMargin.sol";
