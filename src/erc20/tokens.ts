d1e0a7eab94784fb2e2042087e4a64ad772fc787
Kendrick Tan <kendricktan0814@gmail.com>
2020-04-23 19:26:24 +1000

    weth abi

--- src/erc20/tokens.ts
+++ src/erc20/tokens.ts
@@ -1,4 +1,5 @@
 import ERC20Abi from "./abi/ERC20.json";
+import WETHAbi from "./abi/WETH.json";
 
 const tokens = {
   eth: {
@@ -40,7 +41,7 @@ const tokens = {
     symbol: "WETH",
     decimals: 18,
     address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    abi: ERC20Abi,
+    abi: WETHAbi,
   },
   wbtc: {
     symbol: "WBTC",
