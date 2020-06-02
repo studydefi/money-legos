10cf64c737e817f03d0345de7521e5ad5d784da2
Adrian Li <li.adrianmc@gmail.com>
2020-06-02 18:27:22 -0400

    enhance uma script

--- scripts/getDataFromUMA.js
+++ scripts/getDataFromUMA.js
@@ -1,3 +1,46 @@
-// 1. get all JSON files from their artifacts directory
-// 2. filter only for those that have an ABI
-// 3. in a new folder, write JSON files with only the ABI
+const path = require("path");
+const fs = require("fs");
+
+// Usage: node scripts/getDataFromUMA.js <path-to-uma-protocol>
+
+// only include the following contracts
+const fileFilter = [
+  "Finder",
+  "VotingToken",
+  "IdentifierWhitelist",
+  "Voting",
+  "Registry",
+  "FinancialContractsAdmin",
+  "Store",
+  "Governor",
+  "DesignatedVotingFactory",
+  "WETH9",
+  "ExpiringMultiPartyLib",
+  "TokenFactory",
+  "AddressWhitelist",
+  "ExpiringMultiPartyCreator",
+];
+
+// get all JSON files from their artifacts directory
+const inputPath = process.argv[2];
+const artifactPath = path.join(inputPath, "./core/build/contracts");
+const filenames = fs.readdirSync(artifactPath);
+
+// create a fresh temp folder to hold ABIs
+const outputDir = path.join(process.cwd(), "./src/uma/abi");
+if (fs.existsSync(outputDir)) {
+  fs.rmdirSync(outputDir, { recursive: true });
+}
+fs.mkdirSync(outputDir);
+
+// write ABIs to new files
+filenames.forEach((filename) => {
+  const contents = require(path.join(artifactPath, filename));
+  const name = filename.split(".").slice(0, -1).join(".");
+
+  if (fileFilter.includes(name)) {
+    const outputPath = path.join(outputDir, filename);
+    fs.writeFileSync(outputPath, contents.abi);
+    console.log("written:", outputPath);
+  }
+});
