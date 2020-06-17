a78feea851c5b136f5c63c09fabc252bbe1ca9d7
Kris Urbas <krzysu@interia.pl>
2020-06-17 19:50:05 +0200

    add new Idle contracts

--- src/idle/contracts.ts
+++ src/idle/contracts.ts
@@ -1,13 +1,22 @@
 const contracts = {
   maxYield: {
     dai: {
-      address: "0x78751b12da02728f467a44eac40f5cbc16bd7934",
+      address: "0x78751B12Da02728F467A44eAc40F5cbc16Bd7934",
     },
     usdc: {
       address: "0x12B98C621E8754Ae70d0fDbBC73D6208bC3e3cA6",
     },
     usdt: {
       address: "0x63D27B3DA94A9E871222CB0A32232674B02D2f2D",
+    },
+    susd: {
+      address: "0xb39ca0261a1b2986a6a9Fe38d344B56374963dE5",
+    },
+    tusd: {
+      address: "0x7DB7A4a50b26602E56536189Aa94678C80F8E5b6",
+    },
+    wbtc: {
+      address: "0xD6f279B7ccBCD70F8be439d25B9Df93AEb60eC55",
     },
   },
   riskAdjusted: {
@@ -18,7 +27,7 @@ const contracts = {
       address: "0xcDdB1Bceb7a1979C6caa0229820707429dd3Ec6C",
     },
     usdt: {
-      address: "0x42740698959761baf1b06baa51efbd88cb1d862b",
+      address: "0x42740698959761BAF1B06baa51EfBD88CB1D862B",
     },
   },
 };
